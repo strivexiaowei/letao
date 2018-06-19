b4bfe8513e5af27a07ac3f1a6201f983f0b02e78
Huangxianweigit <1024599729@qq.com>
2018-06-19 20:44:42 +0800

    用户管理页面成功

---
+++ public/admin/js/user.js
@@ -0,0 +1,55 @@
+$(function () {
+  var page = 1;
+  var pageSize = 8;
+  render();
+  function render() {
+    $.ajax({
+      type: "get",
+      url: "/user/queryUser",
+      data: {
+        page: page,
+        pageSize: pageSize
+      },
+      success: function (info) {
+        console.log(info);
+        $("#tbody").html(template("tpl1", info))
+        $("#paginator").bootstrapPaginator({
+          bootstrapMajorVersion: 3,
+          currentPage: page,
+          totalPages: Math.ceil(info.total / info.size),
+          size: "small",
+          onPageClicked: function (a, b, c, p) {
+            page = p;
+            render();
+          }
+        })
+      }
+    });
+  }
+
+  $("#tbody").on("click", ".btn", function () {
+    $("#userModal").modal("show");
+    var id = $(this).parent().data("id");
+    var isDelete = $(this).hasClass("btn-confirm") ? 1 : 0;
+    console.log(isDelete);
+    $("#userconfirm").off().on("click", function () {
+      $("#userModal").modal("hide");
+      $.ajax({
+        type: "post",
+        url: "/user/updateUser",
+        data: {
+          id: id,
+          isDelete: isDelete
+        },
+        success: function (info) {
+          if (info.success) {
+            render();
+          }
+
+        }
+      });
+
+    })
+
+  })
+})
