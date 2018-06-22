0d898e34c6ef276bea1f1a087451888de578477f
Huangxianweigit <1024599729@qq.com>
2018-06-22 09:55:03 +0800

    修改

--- public/admin/js/product.js
+++ public/admin/js/product.js
@@ -18,6 +18,36 @@ $(function () {
           currentPage: page,
           totalPages: Math.ceil(info.total / info.size),
           size: "small",
+          itemTexts: function (type, page, current) {
+            switch (type) {
+              case "first":
+                return "首页";
+              case "prev":
+                return "上一页";
+              case "next":
+                return "下一页";
+              case "last":
+                return "尾页";
+              default:
+                return page;
+            }
+          },
+          tooltipTitles: function (type, page, current) {
+            switch (type) {
+              case "first":
+                return "首页";
+              case "prev":
+                return "上一页";
+              case "next":
+                return "下一页";
+              case "last":
+                return "尾页";
+              //如果是page，说明就是数字，只需要返回对应的数字即可
+              default:
+                return "跳转到" + page;
+            }
+          },
+          useBootstrapTooltip: true,
           onPageClicked: function (a, b, c, p) {
             page = p;
             render();
@@ -55,131 +85,131 @@ $(function () {
     dataType: "json",      //e：事件对象      //data：图片上传后的对象，通过e.result.picAddr可以获取上传后的图片地址   
     done: function (e, data) {
       if (imgs.length >= 3) {
-    return false;
-  }
-  imgs.push(data.result);
-  $(".box-img").append(' <img src=" ' + data.result.picAddr + '" width="100" height="100"> ');
-  if (imgs.length === 3) {
-    $("#form").data("bootstrapValidator").updateStatus("brandLogo", "VALID");
-  } else {
-    $("#form").data("bootstrapValidator").updateStatus("brandLogo", "INVALID");
-  }
-}
+        return false;
+      }
+      imgs.push(data.result);
+      $(".box-img").append(' <img src=" ' + data.result.picAddr + '" width="100" height="100"> ');
+      if (imgs.length === 3) {
+        $("#form").data("bootstrapValidator").updateStatus("brandLogo", "VALID");
+      } else {
+        $("#form").data("bootstrapValidator").updateStatus("brandLogo", "INVALID");
+      }
+    }
   });
-$("#form").bootstrapValidator({
-  excluded: [],
-  feedbackIcons: {
-    valid: 'glyphicon glyphicon-ok',
-    invalid: 'glyphicon glyphicon-remove',
-    validating: 'glyphicon glyphicon-refresh'
-  },
-  fields: {
-    brandId: {
-      validators: {
-        notEmpty: {
-          message: '请选择二级分类品牌'
-        }
-      }
+  $("#form").bootstrapValidator({
+    excluded: [],
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
     },
-    proName: {
-      validators: {
-        notEmpty: {
-          message: '商品名称不能为空'
+    fields: {
+      brandId: {
+        validators: {
+          notEmpty: {
+            message: '请选择二级分类品牌'
+          }
         }
-      }
-    },
-    oldPrice: {
-      validators: {
-        notEmpty: {
-          message: '商品原价格不能为空'
-        },
-        regexp: {
-          regexp: /\d/,
-          message: '输入数字'
+      },
+      proName: {
+        validators: {
+          notEmpty: {
+            message: '商品名称不能为空'
+          }
         }
-      }
-    },
-    price: {
-      validators: {
-        notEmpty: {
-          message: '商品新价格不能为空'
-        },
-        regexp: {
-          regexp: /\d/,
-          message: '输入数字'
+      },
+      oldPrice: {
+        validators: {
+          notEmpty: {
+            message: '商品原价格不能为空'
+          },
+          regexp: {
+            regexp: /\d/,
+            message: '输入数字'
+          }
         }
-      }
-    },
-    proDesc: {
-      validators: {
-        notEmpty: {
-          message: '商品描述不能为空'
+      },
+      price: {
+        validators: {
+          notEmpty: {
+            message: '商品新价格不能为空'
+          },
+          regexp: {
+            regexp: /\d/,
+            message: '输入数字'
+          }
         }
-      }
-    },
-    brandLogo: {
-      validators: {
-        notEmpty: {
-          message: '请上传三张图片'
+      },
+      proDesc: {
+        validators: {
+          notEmpty: {
+            message: '商品描述不能为空'
+          }
         }
-      }
-    },
-    size: {
-      validators: {
-        notEmpty: {
-          message: '输入尺码不能为空格式为36-46'
-        },
-        regexp: {
-          regexp: /^\d{2}-\d{2}$/,
-          message: '输入尺码格式为36-46'
+      },
+      brandLogo: {
+        validators: {
+          notEmpty: {
+            message: '请上传三张图片'
+          }
         }
-      }
-    },
-    num: {
-      validators: {
-        notEmpty: {
-          message: '商品库存不能为空'
-        },
-        regexp: {
-          regexp: /^[1-9]\d{0,3}$/,
-          message: '请输入1-9999的数字'
+      },
+      size: {
+        validators: {
+          notEmpty: {
+            message: '输入尺码不能为空格式为36-46'
+          },
+          regexp: {
+            regexp: /^\d{2}-\d{2}$/,
+            message: '输入尺码格式为36-46'
+          }
+        }
+      },
+      num: {
+        validators: {
+          notEmpty: {
+            message: '商品库存不能为空'
+          },
+          regexp: {
+            regexp: /^[1-9]\d{0,3}$/,
+            message: '请输入1-9999的数字'
+          }
         }
       }
     }
-  }
-})
+  })
 
 
-$("#form").on("success.form.bv", function (e) {
+  $("#form").on("success.form.bv", function (e) {
 
-  e.preventDefault();
-  console.log("hehe");
+    e.preventDefault();
+    console.log("hehe");
 
-  var str = $("#form").serialize();
-
-
-  str += "&picName1=" + imgs[0].picName + "&picAddr1=" + imgs[0].picAddr;
-  str += "&picName2=" + imgs[1].picName + "&picAddr2=" + imgs[1].picAddr;
-  str += "&picName3=" + imgs[2].picName + "&picAddr3=" + imgs[2].picAddr;
+    var str = $("#form").serialize();
 
 
-  $.ajax({
-    type: "post",
-    url: "/product/addProduct",
-    data: str,
-    success: function (info) {
-      page = 1;
-      render();
-      $("#productModal").modal("hide");
-      $("#form")[0].reset();
-      $("#form").data("bootstrapValidator").resetForm();
-      $("#brandId").val();
-      $(".dropdown-text").text("请选择二级分类");
-      $(".box-img img").remove();
-      imgs = [];
+    str += "&picName1=" + imgs[0].picName + "&picAddr1=" + imgs[0].picAddr;
+    str += "&picName2=" + imgs[1].picName + "&picAddr2=" + imgs[1].picAddr;
+    str += "&picName3=" + imgs[2].picName + "&picAddr3=" + imgs[2].picAddr;
 
-    }
-  });
-})
+
+    $.ajax({
+      type: "post",
+      url: "/product/addProduct",
+      data: str,
+      success: function (info) {
+        page = 1;
+        render();
+        $("#productModal").modal("hide");
+        $("#form")[0].reset();
+        $("#form").data("bootstrapValidator").resetForm();
+        $("#brandId").val();
+        $(".dropdown-text").text("请选择二级分类");
+        $(".box-img img").remove();
+        imgs = [];
+
+      }
+    });
+  })
 
 })
